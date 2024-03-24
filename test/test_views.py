714c07bdcc0fc252eb700081cd1eb0c1b78f3e9f
vdi-terminal <vdi-terminal@EDU-WSB.local>
2024-03-24 18:02:05 +0100

    usuwam polski znka

--- test/test_views.py
+++ test/test_views.py
@@ -15,4 +15,4 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEqual(b'{ "imie":"Przemysław", "msg":"Hello World!"}', rv.data)
+        self.assertEqual(b'{ "imie":"Przemyslaw", "msg":"Hello World!"}', rv.data)
